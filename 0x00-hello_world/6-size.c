b55c495e10cf17c2e266670100d140206ea6b3dd
Stephen Ford <fordstphn@gmail.com>
2022-09-09 08:51:52 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int i;
 long int l;
 long long int v;
 float f;
-printf("Size of a char: %lu byte\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte\n", (unsigned long)sizeof(l));
-printf("Size of a long long int: %lu byte\n", (unsigned long)sizeof(v));
-printf("Size of a float: %lu byte\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
