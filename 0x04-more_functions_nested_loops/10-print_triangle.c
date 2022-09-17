a8f1664914a9b282e2fe425ffe07d762deb86fcf
Stephen Ford <fordstphn@gmail.com>
2022-09-16 20:36:28 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_tringle - prints a triangle with #
+ * @size: function parameter
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j, n = size - 1;
+		
+		for (i = 0 ; j < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				if (j < n)
+				{
+					_putchar(' ');	
+				}
+				else
+				{
+					putchar('#');
+				}
+				n--;
+				_putchar('\n');
+			}
+		}
+	else
+	{
+		_putchar('\n');
+	}
