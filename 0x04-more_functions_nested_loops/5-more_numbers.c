a3cae8cc2b54d30696417f788534f2b882e6c174
Stephen Ford <fordstphn@gmail.com>
2022-09-16 18:30:28 -0700

    print_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@
 
 void more_numbers(void)
 {
-	int i = 0 ;
+	int i = 0;
 	int j;
 
 	while ( i < 11)
@@ -17,12 +17,11 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar('0' + (j / 10)); 
+				_putchar('0' + j / 10);
 			}
 			_putchar('0' + j % 10);
 		}
 		i++;
-
 		_putchar('\n');
 	}
 }
