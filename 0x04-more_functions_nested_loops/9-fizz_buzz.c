4789aa5b506ae937097c23ea640818671889bae9
Stephen Ford <fordstphn@gmail.com>
2022-09-16 20:20:50 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1 ; i <= 100 ; i++)
+	for (i = 1; i <= 100; i++)
 	{
 
 		if (i % 15 == 0)
@@ -36,4 +36,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
+}ii
