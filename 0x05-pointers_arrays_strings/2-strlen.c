49258209484d6d1bce47b4ba471de4a9820a722b
Stephen Ford <fordstphn@gmail.com>
2022-09-19 01:41:39 -0700

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of the inputted string
+ * @s: parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[1] != '\0'; ++i)
+	{
+	return (i);	
+	}
+}
