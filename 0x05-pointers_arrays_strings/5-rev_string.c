563e2ce3b467480cc36d89cb62aa807708d0cc42
Stephen Ford <fordstphn@gmail.com>
2022-09-19 15:19:49 -0700

    main

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rev_string - print revversed string, followed by a newline
+ * @s: parameter
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	return (strrev(s));
+}
