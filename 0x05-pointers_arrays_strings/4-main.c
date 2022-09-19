6f651668222a40d8c46777dca264300089bb3a3a
Stephen Ford <fordstphn@gmail.com>
2022-09-19 03:46:15 -0700

    Print_reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str;
+	str = "I do not fear computers. I fear the lack of them - Isaac aAsimov";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print strings in reverse order
+ * @s: parameter
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
