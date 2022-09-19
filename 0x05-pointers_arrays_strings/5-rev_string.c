fc4cb76a0ea3aedbd3abf3eb0f96e15b2b9f65f9
Stephen Ford <fordstphn@gmail.com>
2022-09-19 15:51:43 -0700

    main

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,32 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
 /**
  * rev_string - print revversed string, followed by a newline
- * @s: parameter
+ * @is: parameter
  * Return: void
  */
 
 void rev_string(char *s)
 {
-	return (strrev(s));
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	a = a - 1;
+
+	while (b < a + 1 / 2)
+	{
+		c = s[b];
+		s[b] = s[a];
+		s[a] = c;
+
+		a--;
+		b++;
+	}
 }
