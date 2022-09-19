c8dcbcf963c7d54bbd7bafa923b7ae64f49aac6f
Stephen Ford <fordstphn@gmail.com>
2022-09-19 01:02:44 -0700

    swap_values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main: function will swap the value of two integers
+ * @a: parameter
+ * @b: parameter
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = *temp;
+
+	return (0);
+}
+
