fc8baa8debcdea59ddbcd48f5d344008ab13e8f6
Stephen Ford <fordstphn@gmail.com>
2022-09-21 18:29:02 -0700

    Contatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, i;
+	int a = 0, i = 0;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
+	while (dest[i] != '\0')
 	{
+		i++;
+	}
 
-		while (src[a] != '\0')
-		{
-			a++;
-			dest[i] = src[a];
-			i++;
-		}
+	while (src[a] != '\0')
+	{
+		a++;
+		dest[i] = src[a];
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
