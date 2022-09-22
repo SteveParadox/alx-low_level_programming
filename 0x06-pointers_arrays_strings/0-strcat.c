76443f11aeff1e1a7dfc7fd261f8b61c2af6c1e6
Stephen Ford <fordstphn@gmail.com>
2022-09-21 17:20:42 -0700

    Contatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - Code to concatenate two strings
+ * @dest: Parameter
+ * @src: Parameter
+ *
+ * Return: 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	{
+		while (src[a] != '\0')
+		{
+			a++;
+			dest[i] = src[a];
+			i++;
+		}
+	}
+	return (dest);
+}
