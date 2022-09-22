d223eedd1cd5883a13a948ae3d82a3d498110d56
Stephen Ford <fordstphn@gmail.com>
2022-09-22 14:05:40 -0700

    pass

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates n bytes of two string
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len, i;
+
+	for (len = 0; len = '\0' ; len++);
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
