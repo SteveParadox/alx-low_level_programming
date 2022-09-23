6c1c07ca44b9306b6ca0bf9059a39c4c9bc1e724
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-09-23 10:14:54 +0100

    Pass
    Pass

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,29 @@
 #include "main.h"
 
-int check_seperators(char c);
-
 /**
  * cap_string - capitalizes the words of a string
  * @str: string to capitalize words
  * Return: str
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 1;
 
-	while (s[i])
+	if (s[0] >= 'a' && s[0] <= 'z')
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
-		i++;
+		s[0] = s[0] - 32;
 	}
-	return (str);
-}
-
-/**
- * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if seperator, 0 otherwise
- */
-
-int check_seperators(char c)
-{
-	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-		'"', '(', ')', '{', '}' };
-	for (; i < 13; i++)
+	for (; s[i]; i++)
 	{
-		if (c == seperators[i])
-			return (1);
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!'
+		    || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '('
+		     || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'
+		     || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
 	}
-	return (0);
+	return (s);
 }
