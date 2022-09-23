62ec5dda27dd23e00e9b8571599516ddf270a194
Stephen Ford <fordstphn@gmail.com>
2022-09-22 22:44:37 -0700

    pass

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp: compares two strings
+ * _strcmp - compares two strings
  * @s1: parameter
  * @s2: parameter
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _strcmp(char *s1, char *s2)
