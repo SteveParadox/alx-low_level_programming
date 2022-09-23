03b08bc6bd85113d073521dfad200c42d6ac163c
Stephen Ford <fordstphn@gmail.com>
2022-09-22 22:39:37 -0700

    pass

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,10 @@
 
 /**
  * _strcmp: compares two strings
- * *s1: parameter
- * *s2: parameter
- * Return: Comparison
+ * @s1: parameter
+ * @s2: parameter
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int _strcmp(char *s1, char *s2)
