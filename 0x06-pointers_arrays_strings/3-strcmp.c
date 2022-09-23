ff8374e37514c402851aea407ed9bbbc3dc8c476
Stephen Ford <fordstphn@gmail.com>
2022-09-22 22:35:47 -0700

    pass

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	do {
+	while(s1[i] == s2[i] && s1[i] && s2[i])
+	{
 		i++;
-		if (s1[i] != s2[i])
-		
-			return (s1[i] - s2[i]);
-		}
-	}while(s1[i] == s2[i] && s1[i] && s2[i]);
+	}
+
+	return (s1[i] - s2[i]);
 }
