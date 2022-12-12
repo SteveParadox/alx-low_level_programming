6af6d1a7516ddc7adeaeb9f14d917da85ef94f5f
SteveParadox <fordstphn@gmail.com>
2022-12-12 15:10:24 -0800

    main

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
- int i = 122;
-do {
-       putchar(i);
-	i--;
-}
-while (i > 96);
-putchar('\n');
-return 0;
+	int i;
+
+	i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
 }
