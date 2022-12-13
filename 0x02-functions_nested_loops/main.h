dba9fea81f6b942621ad5ea125a1ea9f3b040816
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-12-13 21:11:50 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <ctype.h>
-/*Function to printalphabets in lowercases using putcha()*/
+#ifndef MAIN_H
+#define MAIN_H
+/*Function to print alphabets in lowercases using putchar()*/
 int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void jack_bauer(void);
+void print_times_table(int n);
+#endif
