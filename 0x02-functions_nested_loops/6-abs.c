5a1e56a63e1295fd297a6acf164ae3a01ba66dcf
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-12-13 20:58:59 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absoulute value of a number
+ * @x: function parameter
+ *
+ * Return: x
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (x * -1);
+}
