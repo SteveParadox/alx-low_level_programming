6d79c44305385ee19c41075955a34429d7ca41ba
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-12-13 21:01:34 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
