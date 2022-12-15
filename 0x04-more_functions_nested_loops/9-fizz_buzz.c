cce79d044e3d594a338f5c59aeeb596a46256a59
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2022-12-15 21:29:24 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
 
 /**
- * fizz_ball - Print fizz ball for multiples of 3 and 5 respectively
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: prints numbers from 1 - 100, if the number is a multiple of 3
+ * print Fizz, if a multiple of 5 print Buzz and if a multiple of both FizzBuzz
+ *
+ * Return: Always 0 (success);
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1 ; i <= 100 ; i++)
 	{
-
-		if (i % 15 == 0)
-		{
+		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
+		else if ((i % 3) == 0)
 			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
+		else if ((i % 5) == 0)
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
 		if (i < 100)
-		{
 			putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);
