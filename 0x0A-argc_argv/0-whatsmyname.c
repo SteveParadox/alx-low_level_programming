8d4a48654680313152088a5ed51d6e14e912395c
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-01-12 04:56:32 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,15 @@
-vv
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[--argc]);
+
+	return (0);
+}
