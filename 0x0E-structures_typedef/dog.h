e7fbc4fc2c8c4a99de2795486ee36993d394285e
SteveParadox <fordstphn@gmail.com>
2023-03-03 16:35:40 -0800

    git xx

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: structure that defines properties of dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
